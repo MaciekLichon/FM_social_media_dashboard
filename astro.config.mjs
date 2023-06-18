34c4ccde07a0d2a39a43004e726a1700b9230849
Maciej Lichoń <maciek.lichon@gmail.com>
2023-06-18 20:21:44 +0200

    gh deploy setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-
+  site: 'https://MaciekLichon.github.io',
+  base: '/FM_social_media_dashboard'
 });
